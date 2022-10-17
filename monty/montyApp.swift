9bd56c0a288d729f66be3216b80a5f691768ee5f
Farouq Alsalih <falsalih@u.rochester.edu>
2022-10-17 19:03:21 -0400

    Initial Commit

---
+++ monty/montyApp.swift
@@ -0,0 +1,17 @@
+//
+//  montyApp.swift
+//  monty
+//
+//  Created by Farouq Alsalih on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct montyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
